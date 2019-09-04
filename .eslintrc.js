040cf34e564ecfe8a29a97d6fbbbab9c213ac4ef
LightsOut <kkonstantinov1993@gmail.com>
2019-09-04 21:31:27 +0400

    add vuetify plugin and fix eslint problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,67 @@
+const OFF = 'off';
+const WARN = 'warn';
+const ERROR = 'error';
+
 module.exports = {
   root: true,
   env: {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? ERROR : OFF,
+    'no-debugger': process.env.NODE_ENV === 'production' ? ERROR : OFF,
+
+    // import
+    'import/no-commonjs': ERROR,
+    'import/no-extraneous-dependencies': OFF,
+    'import/extensions': [
+      ERROR,
+      'always',
+      {
+        js: 'never',
+      },
+    ],
+
+    // airbnb-base
+    complexity: [WARN, 11],
+    'no-eq-null': ERROR,
+    'no-magic-numbers': [ERROR, {
+      ignore: [0, -1, 1, 100],
+      ignoreArrayIndexes: true,
+      enforceConst: true,
+      detectObjects: false,
+    }],
+    'no-param-reassign': [ERROR, {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for mutation vuex
+        'acc', // for reduce accumulators
+        'e', // for e.return value
+      ]
+    }],
+
+    // vue
+    'vue/attributes-order': ERROR,
+    'vue/singleline-html-element-content-newline': ERROR,
   },
   parserOptions: {
     parser: 'babel-eslint',
+    parserOptions: {
+      "ecmaVersion": 2017,
+      "sourceType": "module"
+    }
   },
+  overrides: [
+    {
+      files: ['*.config.*'],
+      rules: {
+        'import/no-commonjs': OFF,
+        'global-require': OFF,
+      }
+    },
+  ]
 };
